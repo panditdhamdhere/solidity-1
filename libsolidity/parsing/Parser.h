cb7e0927d5c51b30e185027ed8a6d13dc8de3de2
Matheus Aguiar <matheus.pit@gmail.com>
2024-09-27 21:33:45 -0300

    Parse contract layout specification

--- libsolidity/parsing/Parser.h
+++ libsolidity/parsing/Parser.h
@@ -169,6 +169,8 @@ private:
 	FunctionCallArguments parseFunctionCallArguments();
 	FunctionCallArguments parseNamedArguments();
 	std::pair<ASTPointer<ASTString>, langutil::SourceLocation> expectIdentifierWithLocation();
+
+	ASTPointer<Expression> parseContractStorageBaseLocationExpression();
 	///@}
 
 	///@{
